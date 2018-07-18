93d2a8fcaeb38549f9d3acb21bc1fe5a7a9215a5
Jesus Castaneda <Jesus_Castaneda@symantec.com>
2018-07-18 10:26:57 -0700

    asdf

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,19 @@
 echo -e "Current Directory: "
 pwd
+echo -e "\nItems in Directory:"
 ls
 cd ../
 echo -e "Current Directory: "
 pwd
+echo -e "\nItems in Directory"
 ls
 echo -e "HELLO WORLDS!"
 scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no "wm.tar.gz" "circleci"@"54.197.6.18":/home/"circleci"/
 
 ssh -oStrictHostKeyChecking=no "circleci"@"54.197.6.18" /bin/bash <<EOF
+    echo -e "Successfully SSH'd into server"
+    echo -e "Current Server Directory: "
+    pwd
+    echo -e "\nItems in directory"
     ls
 EOF
