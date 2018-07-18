f251e31304de21c7f929c36cc5d0a493a573c5b4
Jesus Castaneda <Jesus_Castaneda@symantec.com>
2018-07-18 10:24:27 -0700

    asdf

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,13 @@
+echo -e "Current Directory: "
 pwd
 ls
 cd ../
+echo -e "Current Directory: "
+pwd
 ls
 echo -e "HELLO WORLDS!"
+scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no "wm.tar.gz" "circleci"@"54.197.6.18":/home/"circleci"/
+
 ssh -oStrictHostKeyChecking=no "circleci"@"54.197.6.18" /bin/bash <<EOF
-    mkdir -p fromcircleci
+    ls
 EOF
