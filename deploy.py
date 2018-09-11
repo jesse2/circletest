196edefc3df0ff773fe00d32b1266159c75e3935
Jesus Castaneda <Jesus_Castaneda@symantec.com>
2018-09-11 12:08:33 -0700

    python changes

--- deploy.py
+++ deploy.py
@@ -1,10 +1,10 @@
 import os
 
 print("Current Directory: {}".format(os.getcwd()))
-print("Items in Current Directory:\n{}".format(os.listdir))
+print("Items in Current Directory:\n{}".format(os.listdir()))
 pth="/home/circleci"
 print("Switching to circlecli directory")
 os.chdir(pth)
 print("Current Directory: {}".format(os.getcwd()))
-print("Items in Current Directory:\n{}".format(os.listdir))
+print("Items in Current Directory:\n{}".format(os.listdir()))
 
